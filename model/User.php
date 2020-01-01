d8c73f365d8d9e10c701d9b21d2131c7a66fbc0e
Amber96 <ambrozy.pala.21@gmail.com>
2020-01-01 23:19:51 +0100

    Update Model

--- model/User.php
+++ model/User.php
@@ -2,29 +2,59 @@
 
 
 class User{
+    private $id;
     private $email;
     private $password;
     private $name;
     private $surname;
-    private $role = ['ROLE_USER'];
+    private $role;
 
     public function __construct(
-        string $email,
-        string $password,
-        string $name,
-        string $surname
-    ) {
+        $id,
+        $email,
+        $password,
+        $name,
+        $surname,
+        $role) {
+        $this->id = $id;
         $this->email = $email;
         $this->password = $password;
         $this->name = $name;
         $this->surname = $surname;
+        $this->role = $role;
     }
 
-    public function getEmail(): string{
+    public function getEmail(){
         return $this->email;
     }
-
+    public function setEmail($email): void{
+        $this->email = $email;
+    }
     public function getPassword(){
         return $this->password;
+    }
+    public function setPassword($password): void{
+        $this->password = $password;
+    }
+    public function setName($name): void{
+        $this->name = $name;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function setSurname($surname): void{
+        $this->surname = $surname;
+    }
+    public function getSurname(){
+        return $this->surname;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function getRole(){
+        return $this->role;
+    }
+    public function setRole(string $role): void{
+        $this->role = $role;
     }
 }
